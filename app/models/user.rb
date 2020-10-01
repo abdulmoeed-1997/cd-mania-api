7b27fd270ffc2c29e37fdb981a4eda68ba16b9b6
Abdul Moeed <abdul.moeed@devsinc.com>
2020-10-01 00:16:44 -0700

    user.rb: Adding devise:omniauthable for user model + application.rb: Adding session middleware for omniauth to work with api

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :jwt_authenticatable, jwt_revocation_strategy: JwtBlacklist
-  self.skip_session_storage = [:http_auth, :params_auth]
+         :omniauthable, :jwt_authenticatable,
+         omniauth_providers: [:google_oauth2], jwt_revocation_strategy: JwtBlacklist
+  #self.skip_session_storage = [:http_auth, :params_auth]
 end
